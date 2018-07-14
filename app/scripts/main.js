35e2342e0f08b071888827647eed740ff3271cd9
AndreyZaklitskiy <glockman94@gmail.com>
2018-07-14 23:00:17 +0300

    slider-ready

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -20,4 +20,45 @@ $(function() {
       $burger.removeClass( "is-active" );
     }, 100);
   });
+
+  $('.carousel-services').on('initialized.owl.carousel', function() {
+    setTimeout(function() {
+      carouselService()
+    }, 100);
+  });
+
+  $('.carousel-services').owlCarousel({
+    loop: true,
+    nav: true,
+    smartSpeed: 700,
+    navText: ['<i class="fas fa-angle-double-left"></i>', '<i class="fas fa-angle-double-right"></i>'],
+    responsiveClass: true,
+    dots: false,
+    responsive: {
+      0: {items: 1},
+      800: {items: 2},
+      1100: {items: 3}
+    }
+  });
+
+  function carouselService() {
+    $('.carousel-services-item').each(function () {
+      var ths = $(this),
+          thsh = ths.find('.carousel-services-content').outerHeight();
+          ths.find('.carousel-services-image').css('min-height', thsh);
+    });
+  }carouselService();
+
+  $('.carousel-services-composition .h3').each(function () {
+    var ths = $(this);
+    ths.html(ths.html().replace(/(\S+)\s*$/, '<span>$1</span>'));
+  });
+
+  function onResize() {
+    $('.carousel-services-content').equalHeights();
+  }onResize();
+  window.onresize = function(){onResize}
+
 });
+
+
